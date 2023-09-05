7ead39f1c7ef3d8e030b82e9591493edfe39a000
chenjh <>
2023-09-05 20:27:15 +0800

    req参数加构造方法

--- openim-java-sdk/src/main/java/org/ccs/openim/admin/defaultGroup/req/AddDefaultGroupReq.java
+++ openim-java-sdk/src/main/java/org/ccs/openim/admin/defaultGroup/req/AddDefaultGroupReq.java
@@ -1,10 +1,14 @@
 package org.ccs.openim.admin.defaultGroup.req;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AddDefaultGroupReq {
     private List<String> groupIDs;
 }
